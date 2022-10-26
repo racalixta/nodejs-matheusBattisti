3bebbb26454ec923df0b6b33d19eb1ed481f4c69
racalixta <ra.calixta@gmail.com>
2022-10-25 22:24:08 -0300

    add aula 11

---
+++ 11-TOUGHTS/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('toughts2', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+try {
+  sequelize.authenticate();
+  console.log('Autenticamos com sucesso...');
+} catch(err) {
+  console.log(`Não foi possível conectar: ${err}`);
+
+}
+
+module.exports = sequelize;
---
+++ 11-TOUGHTS/routes/toughtsRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const ToughtController = require('../controllers/ToughtController');
+
+// helpe
+const checkAuth = require('../helpers/auth').checkAuth;
+
+
+router.get('/dashboard', checkAuth, ToughtController.dashboard);
+router.get('/', ToughtController.showToughts);
+
+
+
+module.exports = router;
