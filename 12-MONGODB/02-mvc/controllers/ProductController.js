32fb936ba4d6a2ce854fb950e10bdd4806ddd3fc
racalixta <ra.calixta@gmail.com>
2022-11-08 22:14:18 -0300

    novas aulas

---
+++ 12-MONGODB/02-mvc/controllers/ProductController.js
@@ -0,0 +1,12 @@
+const Product = require('../models/Product');
+
+module.exports = class ProductController {
+
+  static showProducts(req, res) {
+    res.render('products/all');
+  }
+
+
+
+
+}
---
+++ 12-MONGODB/04-resgatando-dados/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const conn = require('./db/conn');
+
+const productsRoutes = require('./routes/productsRoutes');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/products', productsRoutes);
+
+
+
+
+
+
+app.listen(3000);
+
+
