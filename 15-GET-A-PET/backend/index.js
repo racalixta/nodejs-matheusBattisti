3d33f31fc7719eaca93c8a2f57e93801d3374f7c
racalixta <ra.calixta@gmail.com>
2022-11-10 21:36:22 -0300

    secao 15 aula 236 e 237

---
+++ 15-GET-A-PET/backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+// config json response
+app.use(express.json());
+
+// solve cors
+app.use(cors({ credentials: true, origin: 'http://localhost:3000'}));
+
+// Public folder for images
+app.use(express.static('public'));
+
+// routes
+
+
+
+
+
+
+app.listen(5000);
